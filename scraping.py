171de1b68a704771a5afe9970515a4b3a3633c86
nagarjuna-de <nagarjuna.de@gmail.com>
2022-02-09 16:56:01 +0100

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -16,7 +16,7 @@ for name in m_name:
     movie_name.append(mo_name)
 #print(movie_name)
 
-# Description
+#Description
 des = soup.find_all('p',class_ = "text-muted")
 
 description = []
@@ -24,96 +24,92 @@ for i in des:
     m_des = i.text.strip()
     description.append(m_des) 
 only_description = description[1::2]
-#print(only_description)
+print(only_description)
 
-# 
+description = []
+# for i in des:
+#     des1 = i.text.strip()
+#     description.append(des1)
+# print(description)
 
-# description = []
-# # for i in des:
-# #     des1 = i.text.strip()
-# #     description.append(des1)
-# # print(description)
+#Duration of the movie.-page-1
+def film_duration():
+    d_time = soup.find_all('span', class_ = "runtime")
+    duration = []
+    for dt in d_time:
+        dur = dt.text
+        duration.append(dur)
+    return duration
+f_d = film_duration()
+print(f_d)
+print(film_duration())
 
+# Rating - Imdb
+imdb_r = soup.find_all('div', class_ = "inline-block ratings-imdb-rating")
+imdb_rating = []
+for ir in imdb_r:
+    r_stars = ir.strong.text
+    imdb_rating.append(r_stars)
+#print(imdb_rating)
 
-# Duration of the movie.-page-1
-# def film_duration():
-#     d_time = soup.find_all('span', class_ = "runtime")
-#     duration = []
-#     for dt in d_time:
-#         dur = dt.text
-#         duration.append(dur)
-#     return duration
-# f_d = film_duration()
-# print(f_d)
-# print(film_duration())
-# print(duration)
+#Rating - Metascore-page-1
+meta_r = soup.find_all('div', class_ = "inline-block ratings-metascore")
+meta_rating =[]
+for mr in meta_r:
+    meta_score = mr.span.text.strip()
+    meta_rating.append(meta_score)
+#print(len(meta_rating))
 
-# # Rating - Imdb
-# imdb_r = soup.find_all('div', class_ = "inline-block ratings-imdb-rating")
-# imdb_rating = []
-# for ir in imdb_r:
-#     r_stars = ir.strong.text
-#     imdb_rating.append(r_stars)
-# #print(imdb_rating)
+# director-name-page1
+d_name = soup.find_all('p', class_ = "")
+director_name =[]
+for dn in d_name:
+    dir_name = dn.a.text.strip()
+    director_name.append(dir_name)
+#print(director_name)
 
-# #Rating - Metascore-page-1
-# meta_r = soup.find_all('div', class_ = "inline-block ratings-metascore")
-# meta_rating =[]
-# for mr in meta_r:
-#     meta_score = mr.span.text.strip()
-#     meta_rating.append(meta_score)
-# #print(len(meta_rating))
+# page-2
+page2 = requests.get("https://www.imdb.com/search/title/?title_type=feature&num_votes=25000,&genres=action&start=51&ref_=adv_nxt")
+soup2 = BeautifulSoup(page2.content, 'html.parser')
 
-# # director-name-page1
-# d_name = soup.find_all('p', class_ = "")
-# director_name =[]
-# for dn in d_name:
-#     dir_name = dn.a.text.strip()
-#     director_name.append(dir_name)
-# #print(director_name)
+# Duration of the movie.-page-2
+d_time2 = soup2.find_all('span', class_ = "runtime")
+duration2 = []
+for dt in d_time2:
+    dur = dt.text
+    duration2.append(dur)
+#print(duration2)
 
-# # page-2
-# page2 = requests.get("https://www.imdb.com/search/title/?title_type=feature&num_votes=25000,&genres=action&start=51&ref_=adv_nxt")
-# soup2 = BeautifulSoup(page2.content, 'html.parser')
+# Rating - Imdb-page2
+imdb_r2 = soup2.find_all('div', class_ = "inline-block ratings-imdb-rating")
+imdb_rating2 = []
+for ir in imdb_r2:
+    r_stars = ir.strong.text
+    imdb_rating2.append(r_stars)
+#print(imdb_rating2)
 
-# # Duration of the movie.-page-2
-# d_time2 = soup2.find_all('span', class_ = "runtime")
-# duration2 = []
-# for dt in d_time2:
-#     dur = dt.text
-#     duration2.append(dur)
-# #print(duration2)
+#Rating - Metascore-page-2
+meta_r2 = soup2.find_all('div', class_ = "inline-block ratings-metascore")
+meta_rating2 =[]
+for mr in meta_r2:
+    meta_score = mr.span.text.strip()
+    meta_rating2.append(meta_score)
+#print(len(meta_rating2))
 
-# # Rating - Imdb-page2
-# imdb_r2 = soup2.find_all('div', class_ = "inline-block ratings-imdb-rating")
-# imdb_rating2 = []
-# for ir in imdb_r2:
-#     r_stars = ir.strong.text
-#     imdb_rating2.append(r_stars)
-# #print(imdb_rating2)
+# director-name-page2
+d_name2 = soup2.find_all('p', class_ = "")
+director_name2 =[]
+for dn in d_name2:
+    dir_name = dn.a.text.strip()
+    director_name2.append(dir_name)
+#print(director_name2)
 
-# #Rating - Metascore-page-2
-# meta_r2 = soup2.find_all('div', class_ = "inline-block ratings-metascore")
-# meta_rating2 =[]
-# for mr in meta_r2:
-#     meta_score = mr.span.text.strip()
-#     meta_rating2.append(meta_score)
-# #print(len(meta_rating2))
+# Intersection of two lists.
 
-# # director-name-page2
-# d_name2 = soup2.find_all('p', class_ = "")
-# director_name2 =[]
-# for dn in d_name2:
-#     dir_name = dn.a.text.strip()
-#     director_name2.append(dir_name)
-# #print(director_name2)
-
-# # Intersection of two lists.
-
-# director = director_name+director_name2
-# imdbrating = imdb_rating+imdb_rating2
-# metarating = meta_rating+meta_rating2
-# film_duration = duration+duration2
+director = director_name+director_name2
+imdbrating = imdb_rating+imdb_rating2
+metarating = meta_rating+meta_rating2
+film_duration = f_d+duration2
 
 # print(director[51])
 # print(len(director))
@@ -121,7 +117,7 @@ only_description = description[1::2]
 # print(len(metarating)) # There was no meta score for pushpa movie.
 # print(len(film_duration))
 
-# pandas data frame.
+#pandas data frame.
 
 
 
